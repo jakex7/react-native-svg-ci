5ede85629598223c3c64ee2be952e40ebfe9d463
Jakub Grzywacz <jakub.grzywacz@swmansion.com>
2025-01-09 16:08:25 +0100

    Revert "chore: update generated specs for the old architecture to work on 0.77 (#2586)"
    This reverts commit 1e1f140b15655f4b1b602c13827ed3e4870b5a08.

--- android/src/paper/java/com/facebook/react/viewmanagers/RNSVGFeCompositeManagerDelegate.java
+++ android/src/paper/java/com/facebook/react/viewmanagers/RNSVGFeCompositeManagerDelegate.java
@@ -12,11 +12,10 @@ package com.facebook.react.viewmanagers;
 import android.view.View;
 import androidx.annotation.Nullable;
 import com.facebook.react.bridge.DynamicFromObject;
-import com.facebook.react.uimanager.BaseViewManager;
 import com.facebook.react.uimanager.BaseViewManagerDelegate;
-import com.facebook.react.uimanager.LayoutShadowNode;
+import com.facebook.react.uimanager.BaseViewManagerInterface;
 
-public class RNSVGFeCompositeManagerDelegate<T extends View, U extends BaseViewManager<T, ? extends LayoutShadowNode> & RNSVGFeCompositeManagerInterface<T>> extends BaseViewManagerDelegate<T, U> {
+public class RNSVGFeCompositeManagerDelegate<T extends View, U extends BaseViewManagerInterface<T> & RNSVGFeCompositeManagerInterface<T>> extends BaseViewManagerDelegate<T, U> {
   public RNSVGFeCompositeManagerDelegate(U viewManager) {
     super(viewManager);
   }
